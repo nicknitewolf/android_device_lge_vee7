430e85a1a1eaff8601c9c04cd78c5be9241d4ae5
nicknitewolf <nicknitewolf@gmail.com>
2017-02-18 12:04:39 +0800

    vee7: cmhw: Update VibratorHW
    Change-Id: Ieb3bbca80288bc3fb81a277b4688681d8992317a

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,9 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
+
+import android.util.Log;
 
 public class VibratorHW {
 
