a6320f707a7b451a22dd5c22d70d0d43831b33f0
nicknitewolf <nicknitewolf@gmail.com>
2017-02-21 00:44:51 +0800

    vee7: audio: Stop using our own policy_hal
     * Our current policy_hal is deprecated
     * Keep on using our own audio_hal
     * Add new board flag to do so
     * Disable certain audio features as these are enabled
       by default in common policy manager, so we need to
       explicitly disable them in order to not build
       support for them.
    
    Change-Id: Ibf543655adf0065695e691486ee8be4e780fb080

--- audio/Android.mk
+++ audio/Android.mk
@@ -1,7 +1,6 @@
 AUDIO_HAL_PATH := $(call my-dir)
 
-ifeq ($(strip $(BOARD_USES_LEGACY_ALSA_AUDIO)),true)
+ifeq ($(strip $(BOARD_USES_LEGACY_MSM8X25_AUDIO)),true)
     include $(AUDIO_HAL_PATH)/audio_hal/Android.mk
-    include $(AUDIO_HAL_PATH)/policy_hal/Android.mk
 endif
  
